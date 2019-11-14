8405aad98e63cd3fcc8a1ec78b9b6eba7a0749df
Jackyjk <2538808265@qq.com>
2019-11-14 12:26:05 +0800

    相关的配置信息

---
+++ finalCode.js
@@ -0,0 +1,10 @@
+/*
+ * @fileheader.Author: Mr.J
+ * @fileheader.LastModifiedBy: Mr.J
+ * @Date: 2019-11-14 11:19:56
+ * @LastEditors: Mr.J
+ * @LastEditTime: 2019-11-14 12:21:16
+ */
+const arr = [1, 2, 3, 4, 5, 6, 99, 8, 78, 9, 9];
+
+const info = websites.map(website => `${website} is cool!`);

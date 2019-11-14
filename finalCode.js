7fe3e6b2a5ce42113689ef4d65bfddf6c907681d
Jackyjk <2538808265@qq.com>
2019-11-14 12:35:09 +0800

    all test demo

--- finalCode.js
+++ finalCode.js
@@ -1,10 +1,14 @@
+"use strict";
+
 /*
  * @fileheader.Author: Mr.J
  * @fileheader.LastModifiedBy: Mr.J
  * @Date: 2019-11-14 11:19:56
  * @LastEditors: Mr.J
- * @LastEditTime: 2019-11-14 12:21:16
+ * @LastEditTime: 2019-11-14 12:30:41
  */
-const arr = [1, 2, 3, 4, 5, 6, 99, 8, 78, 9, 9];
+var arr = [1, 2, 3, 4, 5, 6, 99, 8, 78, 9, 9];
 
-const info = websites.map(website => `${website} is cool!`);
+var info = websites.map(function (website) {
+  return website + " is cool!";
+});
